7a559bf0553f842dd04b627e90c757aae418e49f
nikumyash <98515855+nikumyash@users.noreply.github.com>
2024-02-27 23:24:30 +0530

    added a favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,10 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster"
 
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Jod",
+  title: "Joice",
   description: "Simple and free to use text to speech website.",
 };
 
